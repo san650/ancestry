90c536b6d3e67a45ab95447f792a84b8ce7fe35f
Santiago Ferreira <san650@gmail.com>
2026-03-05 22:07:23 -0300

    Fix waffle_ecto version constraint and dep ordering

--- mix.exs
+++ mix.exs
@@ -60,15 +60,15 @@ defmodule Family.MixProject do
        depth: 1},
       {:swoosh, "~> 1.16"},
       {:req, "~> 0.5"},
+      {:waffle, "~> 1.1"},
+      {:waffle_ecto, "~> 0.0"},
+      {:oban, "~> 2.18"},
       {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"},
-      {:waffle, "~> 1.1"},
-      {:waffle_ecto, "~> 0.0.12"},
-      {:oban, "~> 2.18"}
+      {:bandit, "~> 1.5"}
     ]
   end
 
