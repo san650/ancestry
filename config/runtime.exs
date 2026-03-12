039eacbf5f207b0b640944be0679b4b7a74ee974
Santiago Ferreira <san650@gmail.com>
2026-03-12 14:49:29 -0300

    Use config_env() == :prod guard in runtime.exs

--- config/runtime.exs
+++ config/runtime.exs
@@ -20,7 +20,7 @@ if System.get_env("PHX_SERVER") do
   config :family, Web.Endpoint, server: true
 end
 
-if config_env() != :test do
+if config_env() == :prod do
   config :family, Web.Endpoint, http: [port: String.to_integer(System.get_env("PORT", "4000"))]
 end
 
