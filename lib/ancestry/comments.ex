28112d03f5d205a5732aa2d0dfe4bdc1a253f780
Santiago Ferreira <san650@gmail.com>
2026-03-16 17:44:57 -0300

    Add list, get, update, delete for photo comments

--- lib/ancestry/comments.ex
+++ lib/ancestry/comments.ex
@@ -1,4 +1,5 @@
 defmodule Ancestry.Comments do
+  import Ecto.Query
   alias Ancestry.Repo
   alias Ancestry.Comments.PhotoComment
 
@@ -22,7 +23,53 @@ defmodule Ancestry.Comments do
     end
   end
 
+  def list_photo_comments(photo_id) do
+    Repo.all(
+      from c in PhotoComment,
+        where: c.photo_id == ^photo_id,
+        order_by: [asc: c.inserted_at, asc: c.id]
+    )
+  end
+
+  def get_photo_comment!(id), do: Repo.get!(PhotoComment, id)
+
   def change_photo_comment(%PhotoComment{} = comment, attrs \\ %{}) do
     PhotoComment.changeset(comment, attrs)
+  end
+
+  def update_photo_comment(%PhotoComment{} = comment, attrs) do
+    comment
+    |> PhotoComment.changeset(attrs)
+    |> Repo.update()
+    |> case do
+      {:ok, comment} ->
+        Phoenix.PubSub.broadcast(
+          Ancestry.PubSub,
+          "photo_comments:#{comment.photo_id}",
+          {:comment_updated, comment}
+        )
+
+        {:ok, comment}
+
+      error ->
+        error
+    end
+  end
+
+  def delete_photo_comment(%PhotoComment{} = comment) do
+    Repo.delete(comment)
+    |> case do
+      {:ok, comment} ->
+        Phoenix.PubSub.broadcast(
+          Ancestry.PubSub,
+          "photo_comments:#{comment.photo_id}",
+          {:comment_deleted, comment}
+        )
+
+        {:ok, comment}
+
+      error ->
+        error
+    end
   end
 end
--- test/ancestry/comments_test.exs
+++ test/ancestry/comments_test.exs
@@ -17,6 +17,116 @@ defmodule Ancestry.CommentsTest do
     end
   end
 
+  describe "create_photo_comment/1 validations" do
+    test "rejects empty text" do
+      gallery = gallery_fixture()
+      photo = photo_fixture(gallery)
+
+      assert {:error, changeset} =
+               Comments.create_photo_comment(%{text: "", photo_id: photo.id})
+
+      assert "can't be blank" in errors_on(changeset).text
+    end
+
+    test "rejects nil text" do
+      gallery = gallery_fixture()
+      photo = photo_fixture(gallery)
+
+      assert {:error, changeset} =
+               Comments.create_photo_comment(%{photo_id: photo.id})
+
+      assert "can't be blank" in errors_on(changeset).text
+    end
+  end
+
+  describe "list_photo_comments/1" do
+    test "returns comments ordered oldest first" do
+      gallery = gallery_fixture()
+      photo = photo_fixture(gallery)
+
+      {:ok, first} = Comments.create_photo_comment(%{text: "First", photo_id: photo.id})
+      {:ok, second} = Comments.create_photo_comment(%{text: "Second", photo_id: photo.id})
+
+      comments = Comments.list_photo_comments(photo.id)
+      assert [%{id: id1}, %{id: id2}] = comments
+      assert id1 == first.id
+      assert id2 == second.id
+    end
+
+    test "returns empty list when no comments" do
+      gallery = gallery_fixture()
+      photo = photo_fixture(gallery)
+
+      assert [] == Comments.list_photo_comments(photo.id)
+    end
+
+    test "only returns comments for the given photo" do
+      gallery = gallery_fixture()
+      photo1 = photo_fixture(gallery)
+      photo2 = photo_fixture(gallery)
+
+      {:ok, _} = Comments.create_photo_comment(%{text: "On photo 1", photo_id: photo1.id})
+      {:ok, _} = Comments.create_photo_comment(%{text: "On photo 2", photo_id: photo2.id})
+
+      assert [comment] = Comments.list_photo_comments(photo1.id)
+      assert comment.text == "On photo 1"
+    end
+  end
+
+  describe "get_photo_comment!/1" do
+    test "returns the comment" do
+      gallery = gallery_fixture()
+      photo = photo_fixture(gallery)
+      {:ok, comment} = Comments.create_photo_comment(%{text: "Hello", photo_id: photo.id})
+
+      fetched = Comments.get_photo_comment!(comment.id)
+      assert fetched.id == comment.id
+      assert fetched.text == "Hello"
+    end
+  end
+
+  describe "update_photo_comment/2" do
+    test "updates text" do
+      gallery = gallery_fixture()
+      photo = photo_fixture(gallery)
+      {:ok, comment} = Comments.create_photo_comment(%{text: "Original", photo_id: photo.id})
+
+      assert {:ok, updated} = Comments.update_photo_comment(comment, %{text: "Edited"})
+      assert updated.text == "Edited"
+    end
+
+    test "rejects empty text on update" do
+      gallery = gallery_fixture()
+      photo = photo_fixture(gallery)
+      {:ok, comment} = Comments.create_photo_comment(%{text: "Original", photo_id: photo.id})
+
+      assert {:error, changeset} = Comments.update_photo_comment(comment, %{text: ""})
+      assert "can't be blank" in errors_on(changeset).text
+    end
+  end
+
+  describe "delete_photo_comment/1" do
+    test "deletes the comment" do
+      gallery = gallery_fixture()
+      photo = photo_fixture(gallery)
+      {:ok, comment} = Comments.create_photo_comment(%{text: "Delete me", photo_id: photo.id})
+
+      assert {:ok, _} = Comments.delete_photo_comment(comment)
+      assert_raise Ecto.NoResultsError, fn -> Comments.get_photo_comment!(comment.id) end
+    end
+  end
+
+  describe "cascade delete" do
+    test "deleting a photo deletes its comments" do
+      gallery = gallery_fixture()
+      photo = photo_fixture(gallery)
+      {:ok, comment} = Comments.create_photo_comment(%{text: "Cascade me", photo_id: photo.id})
+
+      {:ok, _} = Ancestry.Galleries.delete_photo(photo)
+      assert_raise Ecto.NoResultsError, fn -> Comments.get_photo_comment!(comment.id) end
+    end
+  end
+
   defp gallery_fixture(attrs \\ %{}) do
     family = family_fixture()
 
