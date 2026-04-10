24a43c32b6da79bc8c3a14ff72d07dcd5e0857da
Santiago Ferreira <san650@gmail.com>
2026-04-10 07:55:43 -0300

    feat: add memory CRUD with Ecto.Multi, mention sync, and PubSub

--- lib/ancestry/memories.ex
+++ lib/ancestry/memories.ex
@@ -1,8 +1,13 @@
 defmodule Ancestry.Memories do
   import Ecto.Query
 
+  alias Ecto.Multi
   alias Ancestry.Repo
   alias Ancestry.Memories.Vault
+  alias Ancestry.Memories.Memory
+  alias Ancestry.Memories.MemoryMention
+  alias Ancestry.Memories.ContentParser
+  alias Ancestry.People.Person
 
   # --- Vaults ---
 
@@ -39,5 +44,119 @@ defmodule Ancestry.Memories do
 
   def change_vault(%Vault{} = vault, attrs \\ %{}) do
     Vault.changeset(vault, attrs)
+  end
+
+  # --- Memories ---
+
+  def list_memories(vault_id) do
+    Repo.all(
+      from m in Memory,
+        where: m.memory_vault_id == ^vault_id,
+        order_by: [desc: m.inserted_at, desc: m.id],
+        preload: [:cover_photo]
+    )
+  end
+
+  def get_memory!(id) do
+    Repo.get!(Memory, id)
+    |> Repo.preload([:cover_photo, memory_mentions: :person])
+  end
+
+  def create_memory(%Vault{} = vault, account, attrs) do
+    vault = Repo.preload(vault, :family)
+    org_id = vault.family.organization_id
+    attrs = Map.new(attrs, fn {k, v} -> {to_string(k), v} end)
+    content = Map.get(attrs, "content", "")
+    {description, person_ids, _photo_ids} = ContentParser.parse(content)
+    valid_person_ids = filter_org_person_ids(person_ids, org_id)
+
+    Multi.new()
+    |> Multi.insert(:memory, fn _ ->
+      %Memory{memory_vault_id: vault.id, inserted_by: account.id}
+      |> Memory.changeset(attrs)
+      |> Ecto.Changeset.put_change(:description, description)
+    end)
+    |> Multi.insert_all(:mentions, MemoryMention, fn %{memory: memory} ->
+      Enum.map(valid_person_ids, fn pid ->
+        %{memory_id: memory.id, person_id: pid}
+      end)
+    end)
+    |> Repo.transaction()
+    |> case do
+      {:ok, %{memory: memory}} ->
+        Phoenix.PubSub.broadcast(Ancestry.PubSub, "vault:#{vault.id}", {:memory_created, memory})
+        {:ok, memory}
+
+      {:error, _failed_operation, failed_value, _changes} ->
+        {:error, failed_value}
+    end
+  end
+
+  def update_memory(%Memory{} = memory, attrs) do
+    memory = Repo.preload(memory, memory_vault: :family)
+    org_id = memory.memory_vault.family.organization_id
+    attrs = Map.new(attrs, fn {k, v} -> {to_string(k), v} end)
+    content = Map.get(attrs, "content", memory.content || "")
+    {description, person_ids, _photo_ids} = ContentParser.parse(content)
+    valid_person_ids = filter_org_person_ids(person_ids, org_id)
+
+    Multi.new()
+    |> Multi.update(:memory, fn _ ->
+      memory
+      |> Memory.changeset(attrs)
+      |> Ecto.Changeset.put_change(:description, description)
+    end)
+    |> Multi.delete_all(:delete_mentions, fn _ ->
+      from mm in MemoryMention, where: mm.memory_id == ^memory.id
+    end)
+    |> Multi.insert_all(:mentions, MemoryMention, fn _ ->
+      Enum.map(valid_person_ids, fn pid ->
+        %{memory_id: memory.id, person_id: pid}
+      end)
+    end)
+    |> Repo.transaction()
+    |> case do
+      {:ok, %{memory: memory}} ->
+        Phoenix.PubSub.broadcast(
+          Ancestry.PubSub,
+          "vault:#{memory.memory_vault_id}",
+          {:memory_updated, memory}
+        )
+
+        {:ok, memory}
+
+      {:error, _failed_operation, failed_value, _changes} ->
+        {:error, failed_value}
+    end
+  end
+
+  def delete_memory(%Memory{} = memory) do
+    case Repo.delete(memory) do
+      {:ok, memory} ->
+        Phoenix.PubSub.broadcast(
+          Ancestry.PubSub,
+          "vault:#{memory.memory_vault_id}",
+          {:memory_deleted, memory}
+        )
+
+        {:ok, memory}
+
+      error ->
+        error
+    end
+  end
+
+  def change_memory(%Memory{} = memory, attrs \\ %{}) do
+    Memory.changeset(memory, attrs)
+  end
+
+  defp filter_org_person_ids([], _org_id), do: []
+
+  defp filter_org_person_ids(person_ids, org_id) do
+    Repo.all(
+      from p in Person,
+        where: p.id in ^person_ids and p.organization_id == ^org_id,
+        select: p.id
+    )
   end
 end
--- test/ancestry/memories_test.exs
+++ test/ancestry/memories_test.exs
@@ -63,4 +63,111 @@ defmodule Ancestry.MemoriesTest do
       assert %Ecto.Changeset{} = Memories.change_vault(vault, %{})
     end
   end
+
+  describe "memories" do
+    setup do
+      org = insert(:organization)
+      family = insert(:family, organization: org)
+      vault = insert(:vault, family: family)
+      account = insert(:account)
+      person = insert(:person, organization: org)
+      gallery = insert(:gallery, family: family)
+      photo = insert(:photo, gallery: gallery)
+      %{org: org, family: family, vault: vault, account: account, person: person, photo: photo}
+    end
+
+    test "list_memories/1 returns memories ordered by inserted_at desc", %{
+      vault: vault,
+      account: account
+    } do
+      m1 = insert(:memory, memory_vault: vault, account: account)
+      m2 = insert(:memory, memory_vault: vault, account: account)
+      result = Memories.list_memories(vault.id)
+      assert [r2, r1] = result
+      assert r2.id == m2.id
+      assert r1.id == m1.id
+    end
+
+    test "get_memory!/1 returns the memory with preloads", %{vault: vault, account: account} do
+      memory = insert(:memory, memory_vault: vault, account: account)
+      result = Memories.get_memory!(memory.id)
+      assert result.id == memory.id
+    end
+
+    test "create_memory/3 creates a memory and generates description", %{
+      vault: vault,
+      account: account
+    } do
+      attrs = %{name: "Summer Trip", content: "<div>We went to the beach.</div>"}
+      assert {:ok, memory} = Memories.create_memory(vault, account, attrs)
+      assert memory.name == "Summer Trip"
+      assert memory.description == "We went to the beach."
+      assert memory.inserted_by == account.id
+    end
+
+    test "create_memory/3 syncs mentions from content", %{
+      vault: vault,
+      account: account,
+      person: person
+    } do
+      html =
+        "<div>Remember <figure data-trix-attachment='{\"contentType\":\"application/vnd.memory-mention\"}'><span data-person-id=\"#{person.id}\">@#{person.given_name}</span></figure></div>"
+
+      assert {:ok, memory} =
+               Memories.create_memory(vault, account, %{name: "Test", content: html})
+
+      memory = Memories.get_memory!(memory.id)
+      assert length(memory.memory_mentions) == 1
+      assert hd(memory.memory_mentions).person_id == person.id
+    end
+
+    test "create_memory/3 drops mentions for people outside the organization", %{
+      vault: vault,
+      account: account
+    } do
+      other_org = insert(:organization)
+      other_person = insert(:person, organization: other_org)
+
+      html =
+        "<figure data-trix-attachment='{\"contentType\":\"application/vnd.memory-mention\"}'><span data-person-id=\"#{other_person.id}\">@Outsider</span></figure>"
+
+      assert {:ok, memory} =
+               Memories.create_memory(vault, account, %{name: "Test", content: html})
+
+      memory = Memories.get_memory!(memory.id)
+      assert memory.memory_mentions == []
+    end
+
+    test "create_memory/3 with blank name returns error", %{vault: vault, account: account} do
+      assert {:error, %Ecto.Changeset{}} = Memories.create_memory(vault, account, %{name: ""})
+    end
+
+    test "update_memory/2 updates and re-syncs mentions", %{
+      vault: vault,
+      account: account,
+      person: person
+    } do
+      {:ok, memory} =
+        Memories.create_memory(vault, account, %{name: "Test", content: "<div>Hello</div>"})
+
+      new_html =
+        "<figure data-trix-attachment='{\"contentType\":\"application/vnd.memory-mention\"}'><span data-person-id=\"#{person.id}\">@#{person.given_name}</span></figure>"
+
+      assert {:ok, updated} = Memories.update_memory(memory, %{content: new_html})
+      updated = Memories.get_memory!(updated.id)
+      assert length(updated.memory_mentions) == 1
+    end
+
+    test "delete_memory/1 deletes the memory", %{vault: vault, account: account} do
+      {:ok, memory} = Memories.create_memory(vault, account, %{name: "Test", content: ""})
+      assert {:ok, _} = Memories.delete_memory(memory)
+      assert_raise Ecto.NoResultsError, fn -> Memories.get_memory!(memory.id) end
+    end
+
+    test "delete_vault/1 cascades to memories", %{vault: vault, account: account} do
+      {:ok, memory} = Memories.create_memory(vault, account, %{name: "Test", content: ""})
+      Memories.delete_vault(vault)
+      assert_raise Ecto.NoResultsError, fn -> Memories.get_memory!(memory.id) end
+    end
+  end
 end
