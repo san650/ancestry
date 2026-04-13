d76be8bf746ba5d77e790fb8a77d06d811b49cae
Santiago Ferreira <san650@gmail.com>
2026-04-13 19:17:24 -0300

    feat: add file_hash field to Photo schema and photo_exists_in_gallery?/2

--- lib/ancestry/galleries/photo.ex
+++ lib/ancestry/galleries/photo.ex
@@ -9,6 +9,7 @@ defmodule Ancestry.Galleries.Photo do
     field :original_filename, :string
     field :content_type, :string
     field :status, :string, default: "pending"
+    field :file_hash, :string
     belongs_to :gallery, Ancestry.Galleries.Gallery
     has_many :photo_comments, Ancestry.Comments.PhotoComment
     has_many :photo_people, Ancestry.Galleries.PhotoPerson
@@ -18,7 +19,14 @@ defmodule Ancestry.Galleries.Photo do
 
   def changeset(photo, attrs) do
     photo
-    |> cast(attrs, [:gallery_id, :original_path, :original_filename, :content_type, :status])
+    |> cast(attrs, [
+      :gallery_id,
+      :original_path,
+      :original_filename,
+      :content_type,
+      :status,
+      :file_hash
+    ])
     |> validate_required([:gallery_id, :original_path, :original_filename, :content_type])
     |> foreign_key_constraint(:gallery_id)
   end
