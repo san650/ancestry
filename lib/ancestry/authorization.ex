4597aba156f5d3efdae2ccce88d3b3282b43d715
Santiago Ferreira <san650@gmail.com>
2026-04-13 18:08:10 -0300

    feat: add can?/3 template helper for centralized permission checks
    Wraps Permit's can/1 |> do?/2 into a convenient can?/3 function that
    handles nil scope gracefully. Imported into html_helpers so it's
    available in all LiveViews, components, and templates.

--- lib/ancestry/authorization.ex
+++ lib/ancestry/authorization.ex
@@ -1,4 +1,19 @@
 defmodule Ancestry.Authorization do
   @moduledoc "Ties Permit permissions to the application."
   use Permit, permissions_module: Ancestry.Permissions
+
+  @doc """
+  Checks whether the given scope has permission to perform `action` on `resource`.
+
+  Wraps Permit's `can/1 |> do?/2` for convenient use in templates:
+
+      <%= if can?(@current_scope, :index, Account) do %>
+        ...
+      <% end %>
+  """
+  def can?(nil, _action, _resource), do: false
+
+  def can?(scope, action, resource) do
+    can(scope) |> do?(action, resource)
+  end
 end
