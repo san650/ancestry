3ba71030b115c7003967a9864c6d3fb1bec7c441
Santiago Ferreira <san650@gmail.com>
2026-05-09 09:12:11 -0300

    Add Ancestry.Commands.UpdatePhotoComment

---
+++ test/ancestry/commands/update_photo_comment_test.exs
@@ -0,0 +1,31 @@
+defmodule Ancestry.Commands.UpdatePhotoCommentTest do
+  use ExUnit.Case, async: true
+  alias Ancestry.Commands.UpdatePhotoComment
+
+  test "new/1 returns {:ok, command} for valid attrs" do
+    assert {:ok, cmd} = UpdatePhotoComment.new(%{photo_comment_id: 7, text: "edit"})
+    assert %UpdatePhotoComment{photo_comment_id: 7, text: "edit"} = cmd
+  end
+
+  test "new/1 rejects missing required fields" do
+    assert {:error, %Ecto.Changeset{} = cs} = UpdatePhotoComment.new(%{})
+    refute cs.valid?
+    assert {"can't be blank", _} = cs.errors[:photo_comment_id]
+    assert {"can't be blank", _} = cs.errors[:text]
+  end
+
+  test "new/1 enforces text length max" do
+    long = String.duplicate("a", 5001)
+    assert {:error, cs} = UpdatePhotoComment.new(%{photo_comment_id: 1, text: long})
+    refute cs.valid?
+    assert {"should be at most %{count} character(s)", _} = cs.errors[:text]
+  end
+
+  test "primary_step/0 == :preloaded" do
+    assert UpdatePhotoComment.primary_step() == :preloaded
+  end
+
+  test "permission/0 == {:update, PhotoComment}" do
+    assert UpdatePhotoComment.permission() == {:update, Ancestry.Comments.PhotoComment}
+  end
+end
